450d299997336970e430996665888ee110152df9
andebugulin <gulin.andrey2005@gmail,com>
2022-01-16 22:34:10 +0300

    add function load_image

--- begin.py
+++ begin.py
@@ -1,4 +1,6 @@
 import pygame
+import os
+import sys
 
 
 def main():
@@ -17,6 +19,23 @@ def main():
 
     pygame.init()
 
+    '''
+    
+    
+    '''
+    def load_image(name):
+        fullname = os.path.join('data', name)
+        # если файл не существует, то выходим
+        if not os.path.isfile(fullname):
+            print(f"Файл с изображением '{fullname}' не найден")
+            sys.exit()
+        image = pygame.image.load(fullname)
+        return image
+
+    '''
+    
+    
+    '''
 
 
     class Board:
@@ -51,7 +70,17 @@ def main():
 
 
         '''
+    class Player:
+        def __init__(self, x, y):
+            self.
 
+
+
+    '''
+
+
+
+    '''
     size = WIDTH, HEIGHT
     screen = pygame.display.set_mode(size)
 
